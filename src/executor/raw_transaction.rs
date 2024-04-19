176af8f275b3103ffe161bb78b4720351a2c430a
Tony Ke <tonykebot@outlook.com>
2024-04-20 00:37:32 +0800

    add some private tx endpoints to raw tx sender

--- src/executor/raw_transaction.rs
+++ src/executor/raw_transaction.rs
@@ -1,4 +1,5 @@
-use alloy::transports::Transport;
+use alloy::providers::ProviderBuilder;
+use alloy::transports::{BoxTransport, Transport};
 use alloy::{
     primitives::{keccak256, Bytes},
     providers::Provider,
@@ -16,6 +17,36 @@ pub struct RawTransactionSender<T> {
 impl<T> RawTransactionSender<T> {
     pub fn new(provider: Arc<dyn Provider<T>>) -> Self {
         Self { provider }
+    }
+}
+
+impl RawTransactionSender<BoxTransport> {
+    pub fn new_http(url: &str) -> Self {
+        let provider = ProviderBuilder::new()
+            .on_http(url.parse().unwrap())
+            .unwrap();
+        let provider = Arc::new(provider.boxed());
+        Self { provider }
+    }
+
+    pub fn new_with_flashbots() -> Self {
+        Self::new_http("https://rpc.flashbots.net/fast")
+    }
+
+    pub fn new_with_bsc_bloxroute() -> Self {
+        Self::new_http("https://bsc.rpc.blxrbdn.com")
+    }
+
+    pub fn new_with_48club() -> Self {
+        Self::new_http("https://rpc-bsc.48.club")
+    }
+
+    pub fn new_with_polygon_bloxroute() -> Self {
+        Self::new_http("https://polygon.rpc.blxrbdn.com")
+    }
+
+    pub fn new_with_arbitrum_sequencer() -> Self {
+        Self::new_http("https://arb1-sequencer.arbitrum.io/rpc")
     }
 }
 
