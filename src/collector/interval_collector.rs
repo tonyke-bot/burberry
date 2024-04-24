045a265463d000bffb0933cb24871bdc560a2ab8
Tony Ke <tonykebot@outlook.com>
2024-04-24 21:46:25 +0800

    add interval collector

---
+++ src/collector/interval_collector.rs
@@ -0,0 +1,28 @@
+use std::time::{Duration, Instant};
+
+use crate::types::{Collector, CollectorStream};
+use async_trait::async_trait;
+
+pub struct IntervalCollector {
+    interval: Duration,
+}
+
+impl IntervalCollector {
+    pub fn new(interval: Duration) -> Self {
+        Self { interval }
+    }
+}
+
+#[async_trait]
+impl Collector<Instant> for IntervalCollector {
+    async fn get_event_stream(&self) -> eyre::Result<CollectorStream<'_, Instant>> {
+        let stream = async_stream::stream! {
+            loop {
+                tokio::time::sleep(self.interval).await;
+                yield Instant::now();
+            }
+        };
+
+        Ok(Box::pin(stream))
+    }
+}
