f6369bf257fc9da15d3d7da0a5b333cac81701c6
Tony Ke <tonykebot@outlook.com>
2024-05-13 14:20:42 +0800

    block collector in poll mode

--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -4,6 +4,7 @@ mod interval_collector;
 mod log_collector;
 mod logs_in_block_collector;
 mod mempool_collector;
+mod poll_full_block_collector;
 
 pub use block_collector::BlockCollector;
 pub use full_block_collector::FullBlockCollector;
@@ -11,3 +12,4 @@ pub use interval_collector::IntervalCollector;
 pub use log_collector::LogCollector;
 pub use logs_in_block_collector::LogsInBlockCollector;
 pub use mempool_collector::MempoolCollector;
+pub use poll_full_block_collector::PollFullBlockCollector;
---
+++ src/collector/poll_full_block_collector.rs
@@ -0,0 +1,60 @@
+use std::sync::atomic::Ordering;
+use std::sync::{atomic::AtomicU64, Arc};
+use std::time::Duration;
+
+use alloy::rpc::types::eth::BlockId;
+use alloy::transports::Transport;
+use alloy::{providers::Provider, rpc::types::eth::Block};
+use async_trait::async_trait;
+use tracing::error;
+
+use crate::types::{Collector, CollectorStream};
+
+pub struct PollFullBlockCollector<T> {
+    provider: Arc<dyn Provider<T>>,
+    interval: Duration,
+    current_block: AtomicU64,
+}
+
+impl<T> PollFullBlockCollector<T> {
+    pub fn new(provider: Arc<dyn Provider<T>>, interval: Duration) -> Self {
+        Self {
+            provider,
+            interval,
+            current_block: AtomicU64::new(0),
+        }
+    }
+}
+
+#[async_trait]
+impl<T: Clone + Transport> Collector<Block> for PollFullBlockCollector<T> {
+    async fn get_event_stream(&self) -> eyre::Result<CollectorStream<'_, Block>> {
+        let stream = async_stream::stream! {
+            loop {
+                match self.provider.get_block(BlockId::latest(), true).await {
+                    Ok(Some(block)) => {
+                        let current_block = block.header.number.unwrap();
+
+                        let old_block = self
+                            .current_block
+                            .fetch_max(current_block, Ordering::Relaxed);
+
+                        if old_block < current_block {
+                            yield block;
+                        }
+                    }
+                    Ok(None) => {
+                        error!("latest block not found");
+                    }
+                    Err(e) => {
+                        error!("fail to get latest block: {e:#}")
+                    }
+                };
+
+                tokio::time::sleep(self.interval).await;
+            }
+        };
+
+        Ok(Box::pin(stream))
+    }
+}
