a4d66b9e60cba3533d63f965533cbf18056b02d3
publicqi <qi@fuzz.land>
2024-12-11 16:30:24 -0800

    use feature for a more general engine

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -1,4 +1,10 @@
 pub mod dummy;
+
+#[cfg(feature = "ethereum")]
 pub mod raw_transaction;
+
+#[cfg(feature = "telegram")]
 pub mod telegram_message;
+
+#[cfg(feature = "ethereum")]
 pub mod transaction;
